fdf3260cabbbf6cec4fb2f12a0335885d303c924
its-a-wrap <patrick_tweed@hotmail.co.uk>
2020-07-12 14:37:36 +0100

    add airbnb linting

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-export const placeHolder = () => {
-    return 0;
-}
+const placeHolder = () => 0;
+
+export default placeHolder;
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,10 +1,10 @@
-import {placeHolder} from './index.js';
 import chai from 'chai';
+import placeHolder from './index.js';
 
-const {expect} = chai;
+const { expect } = chai;
 
 describe('some test', () => {
-    it('SHOULD do something', () => {
-        expect(placeHolder()).to.be.eql(0);
-    });
+  it('SHOULD do something', () => {
+    expect(placeHolder()).to.be.eql(0);
+  });
 });
