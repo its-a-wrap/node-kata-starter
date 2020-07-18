f716303be2b76753cfff679627caef05c9a1baa1
its-a-wrap <patrick_tweed@hotmail.co.uk>
2020-07-18 10:42:00 +0100

    improvements

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-export class Placeholder {
+export class CommentSanitiser {
   constructor() {
-    this.something = 0;
+    this.badWords = ['crap', 'idiot'];
   }
 
-  anotherFunction() {
-    return 1;
+  sanitise(inputString) {
+    let returnString = inputString;
+
+    this.badWords.forEach((word) => {
+      const badWordRegEx = new RegExp(word, 'ig');
+      returnString = returnString.replace(badWordRegEx, '*'.repeat(word.length));
+    });
+
+    const emailRegEx = new RegExp(/\S+@\S+\.\S+\w/, 'g');
+    returnString = returnString.replace(emailRegEx, '<email address>');
+
+    const phoneNumberRegex = new RegExp(/0+7\d{9}/, 'g');
+    returnString = returnString.replace(phoneNumberRegex, '<phone number>');
+
+    return returnString;
   }
 }
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,11 +1,70 @@
 import chai from 'chai';
-import { Placeholder } from './index.js';
+import { CommentSanitiser } from './index.js';
 
 const { expect } = chai;
 
-describe('some test', () => {
-  const placeholder = new Placeholder();
-  it('SHOULD do something', () => {
-    expect(placeholder.anotherFunction()).to.be.eql(1);
+describe('GIVEN CommentSanitiser class', () => {
+  const commentSanitiser = new CommentSanitiser();
+  describe('GIVEN an empty input string', () => {
+    it('SHOULD return an empty string', () => {
+      const inputString = '';
+      expect(commentSanitiser.sanitise(inputString)).to.be.eql('');
+    });
+  });
+
+  describe('GIVEN an input string `James was a crap builder and an idiot`', () => {
+    it('SHOULD replace swear words with *s matching to word length', () => {
+      const inputString = 'James was a crap builder and an idiot';
+      expect(commentSanitiser.sanitise(inputString)).to.be.eql('James was a **** builder and an *****');
+    });
+  });
+
+  describe('GIVEN an input string `James was a CRAP builder and an IDIOT`', () => {
+    it('SHOULD replace swear words with *s matching to word length', () => {
+      const inputString = 'James was a CRAP builder and an IDIOT';
+      expect(commentSanitiser.sanitise(inputString)).to.be.eql('James was a **** builder and an *****');
+    });
+  });
+
+  describe('GIVEN an input string `James has an email of james@hello.com`', () => {
+    it('SHOULD replace swear words with *s matching to word length', () => {
+      const inputString = 'James has an email of james@hello.com';
+      expect(commentSanitiser.sanitise(inputString)).to.be.eql('James has an email of <email address>');
+    });
+  });
+
+  describe('GIVEN an input string `James has an email of james@hello.co.uk`', () => {
+    it('SHOULD replace swear words with *s matching to word length', () => {
+      const inputString = 'James has an email of james@hello.co.uk';
+      expect(commentSanitiser.sanitise(inputString)).to.be.eql('James has an email of <email address>');
+    });
+  });
+
+  describe('GIVEN an input string `James has an email of patrick.tweed@hello.co.uk`', () => {
+    it('SHOULD replace swear words with *s matching to word length', () => {
+      const inputString = 'James has an email of patrick.tweed@hello.co.uk.';
+      expect(commentSanitiser.sanitise(inputString)).to.be.eql('James has an email of <email address>.');
+    });
+  });
+
+  describe('GIVEN an input string `James has an email of patrick.tweed@hello.co.uk`', () => {
+    it('SHOULD replace swear words with *s matching to word length', () => {
+      const inputString = 'patrick.tweed@hello.co.uk patrick.tweed@hello.co.uk patrick.tweed@hello.co.uk patrick.tweed@hello.co.uk';
+      expect(commentSanitiser.sanitise(inputString)).to.be.eql('<email address> <email address> <email address> <email address>');
+    });
+  });
+
+  describe('GIVEN an input string `James has a phone number of 07801567876`', () => {
+    it('SHOULD replace swear words with *s matching to word length', () => {
+      const inputString = 'James has a phone number of 07801567876';
+      expect(commentSanitiser.sanitise(inputString)).to.be.eql('James has a phone number of <phone number>');
+    });
+  });
+
+  describe('GIVEN an input string `James has a phone number of 07801567876 and an email address of patrick.tweed@hello.com crap and IDIOT`', () => {
+    it('SHOULD replace swear words with *s matching to word length, phone numbers and email addresses', () => {
+      const inputString = 'James has a phone number of 07801567876 and an email address of patrick.tweed@hello.com crap and IDIOT';
+      expect(commentSanitiser.sanitise(inputString)).to.be.eql('James has a phone number of <phone number> and an email address of <email address> **** and *****');
+    });
   });
 });
